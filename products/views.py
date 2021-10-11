870909ecd6d82c2ed1589c49f5f13aa3ce34a887
Win_victor <win-victor@mail.ru>
2021-10-11 15:15:30 +0300

    added DRY for templates

--- products/views.py
+++ products/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {'title': "Geekshop"}
+    return render(request, 'products/index.html', context)
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {'title': "Geekshop - Каталог"}
+    return render(request, 'products/products.html', context)
