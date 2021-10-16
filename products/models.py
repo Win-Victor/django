24ba3a205d2abd21388352f91ee8db5eb7722ca0
Win_victor <win-victor@mail.ru>
2021-10-16 17:44:49 +0300

    added __str__ method for models

--- products/models.py
+++ products/models.py
@@ -6,7 +6,11 @@ from django.db import models
 class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)  # varchar(64), уникальные значения
     description = models.TextField(blank=True, null=True)
+
     # text - тип данных, blank=True - может быть пустым, default null, т.е. необязательное для заполнение поле
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -16,4 +20,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)  # цифр до "," и после
     quantity = models.PositiveIntegerField(default=0)  # какое-то неотрицательное число
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
-    # ключ к таблице, действия при удалении (/ SET_NULL / )
+
+    # ForeignKey - ключ к таблице ProductCategory, действия при удалении (/ SET_NULL / )
+    def __str__(self):
+        return f'{self.name} | {self.category.name}'
