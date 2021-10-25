6f415a33a83be15cc9b929531fc58d08ff7c229d
Win_victor <win-victor@mail.ru>
2021-10-25 13:05:44 +0300

    added the logic of adding a product to the cart, taking into account its quantity in the DB

--- baskets/views.py
+++ baskets/views.py
@@ -9,11 +9,15 @@ def basket_add(request, product_id):
     baskets = Basket.objects.filter(user=request.user, product=product)
 
     if not baskets.exists():
-        Basket.objects.create(user=request.user, product=product, quantity=1)
+        if product.quantity > 0:
+            Basket.objects.create(user=request.user, product=product, quantity=1)
+        else:
+            pass
     else:
         basket = baskets.first()
-        basket.quantity += 1
-        basket.save()
+        if product.quantity > basket.quantity:
+            basket.quantity += 1
+            basket.save()
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
 
