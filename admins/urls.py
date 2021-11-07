842fd6e887a3195d936939e3cb47d50ee971d253
Win_victor <win-victor@mail.ru>
2021-11-07 11:09:15 +0300

    completed DeleteView

--- admins/urls.py
+++ admins/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('users/', UserListView.as_view(), name='admin_users'),
     path('users-create/', UserCreateView.as_view(), name='admin_users_create'),
-    path('users-update/<int:id>/', UserUpdateView.as_view(), name='admin_users_update'),
-    path('users-delete/<int:id>/', UserDeleteView.as_view(), name='admin_users_delete')
+    path('users-update/<int:pk>/', UserUpdateView.as_view(), name='admin_users_update'),
+    path('users-delete/<int:pk>/', UserDeleteView.as_view(), name='admin_users_delete')
 ]
