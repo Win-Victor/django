d55171a56ed780faa5138750b5de0a0793cfc3d0
Win_victor <win-victor@mail.ru>
2021-11-07 12:25:56 +0300

    overloaded dispatch() method

--- admins/views.py
+++ admins/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.decorators import user_passes_test
@@ -13,16 +14,34 @@ def index(request):
     return render(request, 'admins/index.html', context)
 
 
+class UserListView(ListView):
+    model = User
+    template_name = 'admins/admin-users-read.html'
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(ListView, self).get_context_data(**kwargs)
+        context['title'] = 'Админ-панель - Пользователи'
+        return context
+
+
 class UserCreateView(CreateView):
     model = User
     form_class = UserAdminRegistrationForm
     success_url = reverse_lazy('admins:admin_users')
     template_name = 'admins/admin-users-create.html'
 
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserCreateView, self).dispatch(request, *args, **kwargs)
 
-class UserListView(ListView):
-    model = User
-    template_name = 'admins/admin-users-read.html'
+    def get_context_data(self, **kwargs):
+        context = super(CreateView, self).get_context_data(**kwargs)
+        context['title'] = 'Админ-панель - Создание пользователей'
+        return context
 
 
 class UserUpdateView(UpdateView):
@@ -30,6 +49,10 @@ class UserUpdateView(UpdateView):
     form_class = UserAdminProfileForm
     template_name = 'admins/admin-users-update-delete.html'
     success_url = reverse_lazy('admins:admin_users')
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserUpdateView, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super(UserUpdateView, self).get_context_data(**kwargs)
@@ -41,6 +64,10 @@ class UserDeleteView(DeleteView):
     model = User
     template_name = 'admins/admin-users-update-delete.html'
     success_url = reverse_lazy('admins:admin_users')
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserDeleteView, self).dispatch(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
         self.object = self.get_object()
